60de99c359adec560e7292b7da8030d94cee7ddc
hugosenari <hugosenari@gmail.com>
2022-03-12 03:42:04 -0300

    chore: re index httpform

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''Http request form parser'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."httpform-master".url = "path:./master";
+  inputs."httpform-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."httpform-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."httpform-0_1_0".url = "path:./0_1_0";
+  inputs."httpform-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."httpform-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."httpform-0_2_0".url = "path:./0_2_0";
+  inputs."httpform-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."httpform-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Http request form parser'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-httpform-master.flake = false;
+  inputs.src-httpform-master.owner = "tulayang";
+  inputs.src-httpform-master.ref   = "refs/heads/master";
+  inputs.src-httpform-master.repo  = "httpform";
+  inputs.src-httpform-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-httpform-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-httpform-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
